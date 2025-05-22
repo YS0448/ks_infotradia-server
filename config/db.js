d4ad55513d9758ba3eeec4ab76385eb9ea2d5104
YS0448 <yogeshsingh0448@gmail.com>
2025-05-23 00:34:24 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        dbName: process.env.dbName,
+        dbName: process.env.DB_NAME,
     });
 ;
     console.log('MongoDB connected');
